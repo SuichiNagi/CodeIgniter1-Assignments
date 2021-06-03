b07aaf4fac9d412399a0f8b8e36eaf23a91c3e06
Aldrei Glenn Nuqui <68434908+SuichiNagi@users.noreply.github.com>
2021-06-03 16:29:01 +0800

    Add files via upload

---
+++ viewexercises/application/views/main/ninjas.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja</title>
+    <link rel="stylesheet" href="/assets/css/ninjas.css">
+</head>
+<body>
+
+<div class="image-cont">
+<?php for($i = 0; $i < $count; $i++){ ?>
+      <img src='/assets/images/ninjas/n1.jpg' alt='n1'>
+<?php } ?>
+</div>
+
+    <div class="image-cont">
+        <img src="/assets/images/ninjas/n1.jpg" alt="n1">
+        <img src="/assets/images/ninjas/n2.jpg" alt="n2">
+        <img src="/assets/images/ninjas/n3.jpg" alt="n3">
+        <img src="/assets/images/ninjas/n4.jpg" alt="n4">
+        <img src="/assets/images/ninjas/n5.jpg" alt="n5">
+    </div>
+
+</body>
+</html>
